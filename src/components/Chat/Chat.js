3c68247bbd881397f3672ba26f63e36cd3aba310
Th3At0nic <islammdrahatul@gmail.com>
2023-05-14 00:55:00 +0600

    initiated server side and client side with socket.io and running successfully both side

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+
+import io from "socket.io-client";
+import "./Chat.css";
+
+const Chat = ({ location }) => {
+  useEffect(() => {
+    const data = queryString.parse(location);
+
+    console.log(location);
+    console.log(data);
+  });
+
+  return <h1>Chat</h1>;
+};
+
+export default Chat;

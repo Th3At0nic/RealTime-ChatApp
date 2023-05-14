5995bf8bf72c663950dddca4017860b52a2ec14c
Th3At0nic <islammdrahatul@gmail.com>
2023-05-14 11:15:25 +0600

    successfully connected the cliet with the server,shows users in or out with name and room

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
-
 import io from "socket.io-client";
 import "./Chat.css";
+import { useLocation } from "react-router-dom";
 
-const Chat = ({ location }) => {
+let socket;
+
+const Chat = () => {
+  const location = useLocation();
+
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const ENDPOINT = "localhost:5000";
+
   useEffect(() => {
-    const data = queryString.parse(location);
+    const { name, room } = queryString.parse(location.search);
 
-    console.log(location);
-    console.log(data);
-  });
+    socket = io(ENDPOINT);
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      // socket.emit("disconnect"); //this is the previous version, gives error now bcz disconnect is reserved word
+      socket.disconnect();
+
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
 
   return <h1>CHAT</h1>;
 };
